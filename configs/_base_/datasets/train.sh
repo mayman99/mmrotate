ea546073fd8d0b16668df25dd900e62b9cf0b2c6
Mohamad <no.future2e1@gmail.com>
2023-09-04 16:13:25 +0300

    update commands

--- configs/_base_/datasets/train.sh
+++ configs/_base_/datasets/train.sh
@@ -9,3 +9,4 @@ mim download mmrotate --config oriented_rcnn_r50_fpn_1x_dota_le90 --dest .
 python demo/image_demo.py demo/demo.jpg oriented_rcnn_r50_fpn_1x_dota_le90.py oriented_rcnn_r50_fpn_1x_dota_le90-6d2b2ce0.pth --out-file result.jpg
 python tools/train.py configs/oriented_rcnn/oriented_rcnn_r50_fpn_1x_dota_le90.py
 python demo/image_demo.py data/blender_proc_sample/trainval/images/4.png work_dirs/oriented_rcnn_r50_fpn_1x_dota_le90/oriented_rcnn_r50_fpn_1x_dota_le90.py work_dirs/oriented_rcnn_r50_fpn_1x_dota_le90/epoch_30.pth --out-file result.jpg
+python demo/image_demo.py data/oobb_cones_dota/trainval/images/15.png work_dirs/oriented_rcnn_r50_fpn_1x_dota_le90/oriented_rcnn_r50_fpn_1x_dota_le90.py work_dirs/oriented_rcnn_r50_fpn_1x_dota_le90/epoch_8.pth --out-file result.jpg
